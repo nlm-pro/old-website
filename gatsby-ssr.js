73e90b03479db80144df4f51691313c1f50ee141
Noel Mace <contact@noelmace.com>
2020-01-05 13:43:13 +0100

    chore: engreve - showCloseButton

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,22 +4,29 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-const React = require("react");
-const safePrefix = require("./src/utils/safePrefix").default;
+const React = require('react');
+const safePrefix = require('./src/utils/safePrefix').default;
 
 exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
+  setHeadComponents([
+    // EN GREVE
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+            var DIGITAL_STRIKE_OPTIONS = {
+                showCloseButtonOnFullPageWidget: true,
+                alwaysShowWidget: true
+            };
+        `
+      }}
+    />,
+    <script src="https://noelmace.github.io/widget-engreve/widget.js" async />
+  ]);
 
-    setHeadComponents([
-        // EN GREVE
-        <script src="https://noelmace.github.io/widget-engreve/widget.js" async></script>
-    ]);
-
-    setPostBodyComponents([
-        <React.Fragment>
-            <script src={safePrefix('assets/js/plugins.js')}/>
-            <script src={safePrefix('assets/js/main.js')}/>
-            
-        </React.Fragment>
-    ]);
-
+  setPostBodyComponents([
+    <React.Fragment>
+      <script src={safePrefix('assets/js/plugins.js')} />
+      <script src={safePrefix('assets/js/main.js')} />
+    </React.Fragment>
+  ]);
 };
